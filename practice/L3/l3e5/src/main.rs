1dd5c74fc2971ad3c26fb301529d98eea6d1659d
Setu Potnis <setupotnis@gmail.com>
2023-01-16 19:41:49 -0500

    add practice folder

---
+++ practice/L3/l3e5/src/main.rs
@@ -0,0 +1,19 @@
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xys";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
